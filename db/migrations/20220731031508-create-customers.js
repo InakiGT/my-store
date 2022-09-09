4f3176073568daf316b7c9582229c07b7494becd
Iñaki García <yarimfuego@gmail.com>
2022-09-08 20:05:43 -0500

    fix migrations

--- db/migrations/20220731031508-create-customers.js
+++ db/migrations/20220731031508-create-customers.js
@@ -1,10 +1,50 @@
 'use strict';
 
-const { CUSTOMER_TABLE, CustomerSchema } = require('../models/customer.model');
+const { DataTypes, Sequelize } = require('sequelize/types');
+const { CUSTOMER_TABLE } = require('../models/customer.model');
+const { USER_TABLE } = require('../models/user.model');
 
 module.exports = {
     async up (queryInterface) {
-      await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
+      await queryInterface.createTable(CUSTOMER_TABLE, {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER,
+        },
+        name: {
+            allowNull: false,
+            type: DataTypes.STRING,
+        },
+        lastName: {
+            allowNull: false,
+            type: DataTypes.STRING,
+            field: 'last_name',
+        },
+        phone: {
+            allowNull: true,
+            type: DataTypes.STRING
+        },
+        createdAt: {
+            allowNull: false,
+            type: DataTypes.DATE,
+            field: 'created_at',
+            defaultValue: Sequelize.NOW,
+        },
+        userId: {
+            allowNull: false,
+            type: DataTypes.INTEGER,
+            unique: true,
+            field: 'user_id',
+            references: {
+                model: USER_TABLE,
+                key: 'id',
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE',
+        }
+    });
     },
 
     async down (queryInterface) {
